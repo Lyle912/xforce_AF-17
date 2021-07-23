d1e364d9cc10ef795d91b6607d08f889bf68ffa2
Lyle Rubin <lylerubin912@gmail.com>
2021-07-23 11:15:22 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          AF-17
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
