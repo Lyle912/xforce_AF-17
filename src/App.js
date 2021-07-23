0298944b8caab20897488328eff8d312bd22bb8d
Lyle Rubin <lylerubin912@gmail.com>
2021-07-23 11:50:25 -0800

    Removed unneeded files and commented which files shouldn't be touched

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+/*
+This is the file that renders everything to the site
+Anything in the return statement will be rendered
+To start the site, in the terminal type: `npm install` and then `npm start`
+Look up: `React JS` if you are confused by what you are seeing
+*/
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           AF-17
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
